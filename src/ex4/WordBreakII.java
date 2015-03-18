9e2cc23a88c4a8da6598d9ad07b71e5cca0cb420
zhangtou88 <zhangkeyi1994@163.com>
2015-03-18 23:21:29 +0800

    ex4 with DP

--- src/ex4/WordBreakII.java
+++ src/ex4/WordBreakII.java
@@ -1,74 +1,37 @@
 package ex4;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class WordBreakII {
 	public List<String> wordBreak(String s, Set<String> dict) {
-		// if(s.length()<1){
-		// return null;
-		// }
-		int stringLength = s.length();
-		String currentWord = "";
-		int[] spaceLocation = new int[stringLength];
-		for (int i = 0; i < stringLength ; i++) {
-			spaceLocation[i] = 0;
+		HashMap<Integer,ArrayList<Integer>> mark=new HashMap<Integer,ArrayList<Integer>>();
+		for(int i=0;i<s.length();i++){
+			mark.put(i,new ArrayList<Integer>());
 		}
-		int currentWordInitial = 0;
-		int currentIndex = 1;// currentIndex>currentWordInitial
-		List<String> completeList = new ArrayList<String>();
-		while (true) {
-			if (currentWordInitial > stringLength - 1) {
-				break;
-			}
-			currentWord = s.substring(currentWordInitial, currentIndex);
-
-			if (currentIndex < stringLength) {//句子未结束
-				if(dict.contains(currentWord)){
-					spaceLocation[currentIndex - 1] = 1;
-					currentWordInitial = currentIndex;
-				}
-				currentIndex++;
-				continue;
-			} else {// 如果句子已经结束
-				int previousInternal = -1;
-				if(dict.contains(currentWord)){
-					String completeString = "";
-				
-					for (int i = 0; i < stringLength; i++) {
-						completeString += s.charAt(i);
-						if (i < stringLength - 1 && spaceLocation[i] == 1) {
-							completeString += " ";
-							if (i < currentWordInitial - 1) {
-								previousInternal = i;
-							} else {
-								spaceLocation[i] = 0;
-							}
-						}
-					}
-					completeList.add(completeString);// 将完成的句子添加空格后加入列表
-				}else{
-					if(currentWordInitial-1>=0){
-						spaceLocation[currentWordInitial-1]=0;
-					}
-					for(int i=stringLength-1;i>=0;i--){
-						if(spaceLocation[i]==1){
-							previousInternal=i;
-							break;
-						}
-					}
-				}
-				if (currentWordInitial == 0) {// 整个句子已被遍历完毕，无需回溯
-					break;
-				}
-				currentIndex = currentWordInitial + 1;
-				currentWordInitial = previousInternal+1;// 回溯
-
+		for(int stop=s.length();stop>=0;stop--){
+			if(stop<s.length()&&mark.get(stop).isEmpty())continue;
+			for(int start=stop-1;start>=0;start--){
+				if(dict.contains(s.substring(start, stop)))
+					mark.get(start).add(stop);
 			}
 		}
-		return completeList;
+		ArrayList<String> result=new ArrayList<String>();
+		collect(mark,0,s,"",result);
+		return result;
+	}
+	public void collect(HashMap<Integer,ArrayList<Integer>> mark,int ind,String s,String path,ArrayList<String> result){
+		for(int stop:mark.get(ind)){
+			String sub=s.substring(ind, stop);
+			String newPath=path+(ind==0?sub:" "+sub);
+			if(stop==s.length())
+				result.add(newPath);
+			else
+				collect(mark,stop,s,newPath,result);
+		}
 	}
 	public static void main(String[] args){
 		String[] dic={"a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"};
